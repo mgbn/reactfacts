a78221f71f97da244bc38ab651681c4ecc758a1e
mgbiz <mgbizci@gmail.com>
2024-02-06 19:52:48 -0500

    Static project-2

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import img from './logo192.png';
+
+export default function Navbar() {
+  return (
+    <nav>
+        <img src={img}  />
+        <h3>React Facts</h3>
+        <h4>React Course-Project 1</h4>
+
+      
+    </nav>
+  )
+}
